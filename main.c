bd508d7553d04f656341c27bbde27559ea619f13
AhmetKaganKaya <kagan.kaya@ug.bilkent.edu.tr>
2022-05-12 21:42:56 +0300

    part1 is solved

--- main.c
+++ main.c
@@ -4,9 +4,23 @@
 // #include "hw2_output.h"
 #include "hw2_output.c"
 #include <semaphore.h>
+#include <time.h>
+
+void delay(int milliseconds)
+{
+    long pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
+}
 
 int** cig_butts;
-sem_t** mutex;
+sem_t** cig_butts_inter;
+sem_t lock;
+sem_t prior;
 enum hw2_actions action;
 struct Private_struct
 {
@@ -18,36 +32,58 @@ struct Private_struct
     int** top_corners;
 };
 
-void *collect_cigg_butts( struct Private_struct private_struct)
+void *collect_cigg_butts( void* arguments)
 {
-    hw2_init_notifier();
-    hw2_notify(action = GATHERER_CREATED, private_struct.gid, 0, 0);
-    for(int num = 0; num < private_struct.ng; num++)
+    struct Private_struct *private_prop = arguments;
+    hw2_notify(action = GATHERER_CREATED, (*private_prop).gid, 0, 0);
+    for(int num = 0; num < (*private_prop).ng; num++)
     {
-        for(int i = 0; i < private_struct.si; i++ )
+        sem_wait(&prior);
+        for(int i = 0; i < (*private_prop).si; i++ )
         {
-            for (int j = 0; j < private_struct.sj; j++)
+            for (int j = 0; j < (*private_prop).sj; j++)
             {
-                sem_wait(&mutex[private_struct.top_corners[i][0] + i][private_struct.top_corners[i][1] + j]);
+                int coord_i = (*private_prop).top_corners[num][0] + i;
+                int coord_j = (*private_prop).top_corners[num][1] + j;
+                sem_wait(&cig_butts_inter[coord_i][coord_j]);
             }
             
         }
-        hw2_notify(action = GATHERER_ARRIVED, private_struct.gid, private_struct.top_corners[0],private_struct.top_corners[1]);
-        for(int i = 0; i < private_struct.si; i++ )
+        sem_post(&prior);
+        hw2_notify(action = GATHERER_ARRIVED, (*private_prop).gid, (*private_prop).top_corners[num][0],(*private_prop).top_corners[num][1]);
+        for(int j = 0; j < (*private_prop).sj; j++)
         {
-            for (int j = 0; j < private_struct.sj; j++)
+            for(int i = 0; i < (*private_prop).si; i++ )
             {
-                while(cig_butts[private_struct.top_corners[i][0] + i][private_struct.top_corners[i][1] + j] != 0)
+                int coord_i = (*private_prop).top_corners[num][0] + i;
+                int coord_j = (*private_prop).top_corners[num][1] + j;
+                while(cig_butts[coord_i][coord_j] != 0)
                 {
-                    
+                    usleep((*private_prop).time);
+                    sem_wait(&lock);
+                    cig_butts[coord_i][coord_j]--;
+                    hw2_notify(action = GATHERER_GATHERED, (*private_prop).gid, coord_i,coord_j);
+                    sem_post(&lock);
                 }
             }
             
         }
 
+        for(int i = 0; i < (*private_prop).si; i++ )
+        {
+            for (int j = 0; j < (*private_prop).sj; j++)
+            {
+                int coord_i = (*private_prop).top_corners[num][0] + i;
+                int coord_j = (*private_prop).top_corners[num][1] + j;
+                sem_post(&cig_butts_inter[coord_i][coord_j]);
+            }
+            
+        }
+        hw2_notify(action = GATHERER_CLEARED, (*private_prop).gid, 0, 0);
     }
-    
+    hw2_notify(action = GATHERER_EXITED, (*private_prop).gid, 0, 0);
 }
+
 
 int main()
 {
@@ -55,17 +91,19 @@ int main()
     int num_privates;
     scanf("%d %d", &gridx, &gridy);
     cig_butts = (int**)malloc(gridx * sizeof(int*));
-    mutex = (sem_t**)malloc(gridx * sizeof(sem_t*));
+    cig_butts_inter = (sem_t**)malloc(gridx * sizeof(sem_t*));
     for(int i = 0; i < gridx; i++)
     {
         cig_butts[i] = (int*)malloc(gridy * sizeof(int));
-        mutex[i] = (sem_t*)malloc(gridy * sizeof(sem_t));
+        cig_butts_inter[i] = (sem_t*)malloc(gridy * sizeof(sem_t));
         for(int j = 0; j < gridy; j++)
         {
             scanf("%d", &cig_butts[i][j]);
-            sem_init(&mutex[i][j], 0, 1);
+            sem_init(&cig_butts_inter[i][j], 0, 1);
         }
     }
+    sem_init(&lock, 0, 1);
+    sem_init(&prior, 0, 1);
     scanf("%d", &num_privates);
     struct Private_struct privates[num_privates];
     pthread_t private_thread[num_privates];
@@ -79,10 +117,11 @@ int main()
             scanf("%d %d", &privates[i].top_corners[j][0], &privates[i].top_corners[j][1]);
         }
     }
-
+    
+    hw2_init_notifier();
     for(int i = 0; i < num_privates; i++)
     {
-        pthread_create( &private_thread[i], NULL, collect_cigg_butts(privates[i]), NULL);
+        pthread_create( &private_thread[i], NULL, &collect_cigg_butts, (void*)&privates[i]);
     }
 
     for(int i = 0; i < num_privates; i++)
